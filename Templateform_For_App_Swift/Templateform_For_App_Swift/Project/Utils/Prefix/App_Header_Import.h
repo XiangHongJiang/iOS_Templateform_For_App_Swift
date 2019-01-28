a6d6e867e78c8e3d01d3259afc11e4b7f42c248b
MrYeL <mryelee@163.com>
2019-01-28 17:46:35 +0800

    添加 pch

---
+++ Templateform_For_App_Swift/Templateform_For_App_Swift/Project/Utils/Prefix/App_Header_Import.h
@@ -0,0 +1,34 @@
+//
+//  App_Header_Import.h
+//  App_General_Template
+//
+//  Created by JXH on 2017/6/27.
+//  Copyright © 2017年 JXH. All rights reserved.
+//
+
+/** 头文件:需要全局使用的头文件*/
+
+#ifndef App_Header_Import_h
+#define App_Header_Import_h
+
+//Cocoapods：lib
+#pragma mark - Cocoapods
+
+
+//Categorys:分类
+#pragma mark - Categorys
+
+
+
+//ViewControlers:控制器
+#pragma mark - ViewControlers
+
+
+
+
+//Others:其他
+#pragma mark - Others
+
+
+
+#endif /* App_Header_Import_h */
---
+++ Templateform_For_App_Swift/Templateform_For_App_Swift/Project/Utils/Prefix/App_Header_KeyMap.h
@@ -0,0 +1,17 @@
+//
+//  App_Header_KeyMap.h
+//  App_General_Template
+//
+//  Created by JXH on 2017/6/27.
+//  Copyright © 2017年 JXH. All rights reserved.
+//
+
+#ifndef App_Header_KeyMap_h
+#define App_Header_KeyMap_h
+
+#pragma mark - AppBaseTabBarController
+
+
+#pragma mark - UINavigationController+RoutePush.h
+
+#endif /* App_Header_KeyMap_h */
